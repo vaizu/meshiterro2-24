4998b97e9123aa6114e593310ef0ec4602f24b18
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2022-01-15 03:16:41 +0000

    [Remake]4-18

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  
   has_one_attached :profile_image
 
   def get_profile_image(size)
